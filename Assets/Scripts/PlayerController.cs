9851b9f14560e10fdb5859b32dcbaf203487ac8f
Md Kamruzzaman Tusher <8888749+tkamruzzaman@users.noreply.github.com>
2026-02-07 12:30:02 +0200

    playerinput

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private InputActionAsset _inputActionAsset;
+    private InputAction moveAction;
+    private InputAction InteractAction;
+    
+    private Vector2 moveInput;
+    
+    void Awake()
     {
-        
+        //Identify player
+        var playerInput = GetComponent<PlayerInput>();
+        Debug.Log("Player index: " + playerInput.playerIndex);
     }
 
-    // Update is called once per frame
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        if (context.performed)
+        {
+            Debug.Log($"{gameObject.name} MOVED!");
+        }
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log($"{gameObject.name} INTERACTED!!");
+        }
+    }
+
     void Update()
     {
-        
+        // Example movement
+        transform.Translate(moveInput * (Time.deltaTime * 5f));
     }
 }
